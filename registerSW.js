42d668c687b06a0a484fccedb899abe43822aae8
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2025-12-31 06:46:53 +0000

    deploy: 5c31db99c4bf0886e3226174cab18facc0e060e8

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/yuenyuen2010.github.io/ionic_demo/sw.js', { scope: '/yuenyuen2010.github.io/ionic_demo/' })})}
