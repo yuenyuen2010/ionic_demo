a3bb97e49b79dd55b4b64312cdcb716b93781654
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-02 14:36:26 +0000

    deploy: 2e2a083420a59961bed5c0a0c7b8a6ffa01b628e

--- assets/ios.transition-legacy-DN322uDH.js
+++ assets/ios.transition-legacy-DN322uDH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-etXOavDj.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-7t1J2qZY.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
