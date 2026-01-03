610ac2262592cbd16a49388ea54ab3fdfb123983
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-03 18:30:30 +0000

    deploy: 3744660be8c871cdb21cfb8933fc8867f809106f

--- assets/ios.transition-legacy-COBB-ypu.js
+++ assets/ios.transition-legacy-COBB-ypu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DKo4wjKg.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-hRyD75Hn.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
