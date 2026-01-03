230010e5a0ef0ab76fb8d12c89a90155b833b1fc
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-03 08:41:45 +0000

    deploy: 1a910b8017e4518c56ff934bcaf740903b64cc7d

--- assets/ios.transition-legacy-CPShBNQt.js
+++ assets/ios.transition-legacy-CPShBNQt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Rqjjm0Ct.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CpOIrHOv.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
