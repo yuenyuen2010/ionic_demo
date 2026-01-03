eaae5025690c09208c2ef2e71e4ecc4b19a37ac8
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-03 05:30:25 +0000

    deploy: 80537810b460a91473d17f3b7702ee0f73fc17c8

--- assets/ios.transition-legacy-DKreqtYS.js
+++ assets/ios.transition-legacy-DKreqtYS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-De9MnBWs.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CxfN9a_8.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
