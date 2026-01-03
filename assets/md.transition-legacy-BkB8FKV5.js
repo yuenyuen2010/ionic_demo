63d3c1fdc149bc9647c3f47c41e1f613f8dd41ba
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-03 15:02:25 +0000

    deploy: b0504c48ef5c7b96e5702eb8e44b9e03663821d7

--- assets/md.transition-legacy-BkB8FKV5.js
+++ assets/md.transition-legacy-BkB8FKV5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CpOIrHOv.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BfjtuFUq.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
