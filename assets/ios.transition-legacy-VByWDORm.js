8664f90e6f291694707ab691573a9edf482dc9ce
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-04 03:48:46 +0000

    deploy: 03aecc911fab69d9e74d610563c560f36330c6ac

--- assets/ios.transition-legacy-VByWDORm.js
+++ assets/ios.transition-legacy-VByWDORm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C1on5WnF.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BBskCdxG.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
