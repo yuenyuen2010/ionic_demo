11fa1bc2dee4fa5ae71be57b2f7bb71271a40e56
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-04 03:19:39 +0000

    deploy: f1f4dbf335ac56a7642a80dadea53d5a1df49b12

--- assets/swipe-back-legacy-CBJeGnOR.js
+++ assets/swipe-back-legacy-CBJeGnOR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-XFqOQbVq.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-re3wMAqx.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
