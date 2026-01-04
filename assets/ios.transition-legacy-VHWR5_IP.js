0e977e0c22bc0ffeea7f8a58d8b72a52115afb46
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-04 04:04:19 +0000

    deploy: 1a4d7aa8a7eb02d97f245c3aca9f9bae19ce22a3

--- assets/ios.transition-legacy-VHWR5_IP.js
+++ assets/ios.transition-legacy-VHWR5_IP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BBskCdxG.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Cc2u-onK.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
