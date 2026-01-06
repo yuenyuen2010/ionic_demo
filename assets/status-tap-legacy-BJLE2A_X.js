1f4007d08094f29dd079efdd8ce1579501331bd6
yuenyuen2010 <yuenyuen2010@users.noreply.github.com>
2026-01-06 03:12:48 +0000

    deploy: fc17a4cae1ec5c44aa0d1a5547e818ea4d5ed785

--- assets/status-tap-legacy-BJLE2A_X.js
+++
@@ -1,5 +0,0 @@
-System.register(["./index-legacy-1dHJAteT.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.J,s=e.K,o=e.L,i=e.M}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-e("startStatusTap",()=>{const e=window;e.addEventListener("statusTap",()=>{n(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise(e=>s(c,e)).then(()=>{o(async()=>{c.style.setProperty("--overflow","hidden"),await i(c,300),c.style.removeProperty("--overflow")})})})})})}}});
